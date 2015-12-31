ac6e8cd6e2673a7f41d9e1ab64834afd5db08823
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:23:14 +1300

    Converted to PSR-2

--- code/admin/AdvancedReportsAdminItemRequest.php
+++ code/admin/AdvancedReportsAdminItemRequest.php
@@ -2,57 +2,60 @@
 /**
  * Handles requests for managing individual advanced report instances.
  */
-class AdvancedReportsAdminItemRequest extends GridFieldDetailForm_ItemRequest {
+class AdvancedReportsAdminItemRequest extends GridFieldDetailForm_ItemRequest
+{
 
-	private static $allowed_actions = array(
-		'ItemEditForm'
-	);
+    private static $allowed_actions = array(
+        'ItemEditForm'
+    );
 
-	public function ItemEditForm() {
-		$form = parent::ItemEditForm();
+    public function ItemEditForm()
+    {
+        $form = parent::ItemEditForm();
 
-		if($this->record->isInDB() && $this->record->canGenerate()) {
-			$form->Actions()->merge(array(
-				FormAction::create('preview', '')
-					->setTitle(_t('AdvancedReport.PREVIEW', 'Preview'))
-					->setAttribute('data-icon', 'preview')
-					->setAttribute('type', 'button'),
-				FormAction::create('generate', '')
-					->setTitle(_t('AdvancedReport.GENERATE', 'Generate'))
-			));
-		}
+        if ($this->record->isInDB() && $this->record->canGenerate()) {
+            $form->Actions()->merge(array(
+                FormAction::create('preview', '')
+                    ->setTitle(_t('AdvancedReport.PREVIEW', 'Preview'))
+                    ->setAttribute('data-icon', 'preview')
+                    ->setAttribute('type', 'button'),
+                FormAction::create('generate', '')
+                    ->setTitle(_t('AdvancedReport.GENERATE', 'Generate'))
+            ));
+        }
 
-		return $form;
-	}
+        return $form;
+    }
 
-	public function preview($data, $form) {
-		$data = $form->getData();
-		$format = $data['PreviewFormat'];
+    public function preview($data, $form)
+    {
+        $data = $form->getData();
+        $format = $data['PreviewFormat'];
 
-		$result = $this->record->createReport($format);
+        $result = $this->record->createReport($format);
 
-		if($result->content) {
-			return $result->content;
-		} else {
-			return SS_HTTPRequest::send_file(
-				file_get_contents($result->filename), "$data[GeneratedReportTitle].$format"
-			);
-		}
-	}
+        if ($result->content) {
+            return $result->content;
+        } else {
+            return SS_HTTPRequest::send_file(
+                file_get_contents($result->filename), "$data[GeneratedReportTitle].$format"
+            );
+        }
+    }
 
-	public function generate($data, $form) {
-		$report = $this->record;
+    public function generate($data, $form)
+    {
+        $report = $this->record;
 
-		if(!empty($data['GeneratedReportTitle'])) {
-			$title = $data['GeneratedReportTitle'];
-		} else {
-			$title = $report->Title;
-		}
+        if (!empty($data['GeneratedReportTitle'])) {
+            $title = $data['GeneratedReportTitle'];
+        } else {
+            $title = $report->Title;
+        }
 
-		$report->GeneratedReportTitle = $title;
-		$report->prepareAndGenerate();
+        $report->GeneratedReportTitle = $title;
+        $report->prepareAndGenerate();
 
-		return Controller::curr()->redirect($this->Link());
-	}
-
+        return Controller::curr()->redirect($this->Link());
+    }
 }
